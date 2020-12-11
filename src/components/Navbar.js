cbd5e1cf0e573429f67924c7e997cac0bbd67a9b
Joel <joe_lim95@hotmail.com>
2020-12-11 17:52:48 +0800

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import {Link } from "react-router-dom";
-import {Navbar, NavLink} from 'reactstrap';
+import {NavbarBrand, Navbar, NavLink} from 'reactstrap';
 import LoginModal from '../pages/LoginModal.js';
 import {useState} from 'react';
 
@@ -11,15 +11,21 @@ export default ({loggedIn,setLoggedIn}) => {
     setLoggedIn(localStorage.removeItem("jwt"))
 
     return <Navbar className="justify-content-start pt-16 pb-16 pr-0 pl-0" color="light">
+                <NavbarBrand href="/" className="text-muted">
+                    <h1>Nextagram</h1>
+                </NavbarBrand>
                 <Link to="/" className="text-muted">
                     <h3>Homepage</h3>
                 </Link>
-                <Link to="/profile" className="text-muted">
-                    <h3>My Profile</h3>
-                </Link>
+                {loggedIn
+                ?   <Link to="/profile" className="text-muted">
+                        <h3>My Profile</h3>
+                    </Link>
+                :   null
+                }
                 {
                 loggedIn
-                ?  <NavLink>
+                ?   <NavLink>
                         <h3 onClick={logout}>Logout</h3>
                     </NavLink>
                 :   <Link>
